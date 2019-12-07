f53775a2fc0e64291c39045ce1f3bcda161aa841
janniks <jannik.siebert@gmail.com>
2019-12-07 15:14:14 +0100

    Fix (im)port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello CI!"));
 
-app.listen(process.env.port || 3000, () =>
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
   console.log(`CI example app listening on port ${port}!`)
 );
